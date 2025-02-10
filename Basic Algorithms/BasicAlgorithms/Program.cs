d1c40782b5f5a7ed2295a9880f97bcb560467159
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2025-02-10 20:05:39 +0200

    Add the exercises

---
+++ Basic Algorithms/BasicAlgorithms/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace BasicAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Console.WriteLine(Sum(arr, 0));
+        }
+
+        static long Sum(int[] arr, int index)
+        {
+            if (index == arr.Length - 1)
+            {
+                return arr[index];
+            }
+
+            return arr[index] + Sum(arr, index + 1);
+        }
+    }
+}
---
+++ Basic Algorithms/RecursiveFactoriel/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace RecursiveFactoriel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Factorial(n));
+        }
+        
+        static long Factorial(int n)
+        {
+            if (n == 1)
+            {
+                return n;
+            }
+
+            return n * Factorial(n - 1);
+        }
+
+
+    }
+}
