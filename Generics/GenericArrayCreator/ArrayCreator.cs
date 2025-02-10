8caa4774885deca084a18df28b429081bafeb885
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2025-02-10 19:58:38 +0200

    Add the exercises

---
+++ Generics/GenericArrayCreator/ArrayCreator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericArrayCreator
+{
+    public static class ArrayCreator
+    {
+        public static T[] Create<T>(int length, T item)
+        {
+            var result = new T[length];
+            for (int i = 0; i < length; i++)
+            {
+                result[i] = item;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Generics/Generics/Box.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        public List<T> data;
+        public int Count
+        {
+            get { return this.data.Count; }
+        }
+
+        public Box()
+        {
+            this.data = new List<T>();
+        }
+
+        public void Add(T element)
+        {
+            this.data.Add(element);
+        }
+
+        public T Remove()
+        {
+            var rem = this.data.Last();
+            this.data.RemoveAt(this.data.Count - 1);
+            return rem;
+        }
+    }
+}
