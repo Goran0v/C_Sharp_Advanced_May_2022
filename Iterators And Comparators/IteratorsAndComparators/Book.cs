59ae4b53e9f559315d9268913a9b1097de9e0098
Lyudmil Goranov <102062855+Goran0v@users.noreply.github.com>
2025-02-10 20:01:39 +0200

    Add the exercises

---
+++ Iterators And Comparators/IteratorsAndComparators/Book.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public List<string> Authors { get; set; }
+
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = authors.ToList();
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Year}";
+        }
+
+        public int CompareTo([AllowNull] Book other)
+        {
+            if (other == null)
+            {
+                return -1;
+            }
+            int titleCompare = this.Year.CompareTo(other.Year);
+
+            if (titleCompare == 0)
+            {
+                titleCompare = this.Title.CompareTo(other.Title);
+            }
+            return titleCompare;
+        }
+    }
+}
